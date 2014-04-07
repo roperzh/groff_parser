214cbfa6aa449d11406932dc3e6d0035832e45d7
Roberto Dip <dip.jesusr@gmail.com>
2014-04-07 02:18:35 -0300

    Test Suite: enables the usage of context like describe

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -20,3 +20,12 @@ require 'minitest/autorun'
 $:.unshift 'lib'
 require 'groff_parser/document'
 require 'groff_parser/engine'
+
+# Allow usage of 'context' like 'describe'
+module MiniTest
+  class Spec
+    class << self
+      alias_method :context, :describe
+    end
+  end
+end
